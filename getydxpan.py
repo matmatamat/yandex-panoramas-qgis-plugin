38779fb7d14fb203f77d8c5283d060d2f56dda9a
matmatamat <91154741+matmatamat@users.noreply.github.com>
2026-06-26 16:43:53 +0300

    Update getydxpan.py

--- getydxpan.py
+++ getydxpan.py
@@ -2,7 +2,7 @@
 from qgis.utils import iface
 from qgis.gui import QgsMapToolEmitPoint, QgsRubberBand
 from qgis.core import QgsPointXY, QgsWkbTypes, QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsProject
-from qgis.PyQt.QtCore import Qt, QUrl, 
+from qgis.PyQt.QtCore import Qt, QUrl
 from qgis.PyQt.QtGui import QColor, QDesktopServices
 from math import atan2, degrees, log
 import webbrowser
